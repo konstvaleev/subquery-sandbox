17e82451823ddfc9294a03c5a27277dc8db8fc6d
Valeev <konstantin.valeev@wirecard.com>
2022-01-30 17:02:41 +0300

    removed sumreward

--- tutorials-simple-aggregation/src/mappings/mappingHandlers.ts
+++ tutorials-simple-aggregation/src/mappings/mappingHandlers.ts
@@ -2,12 +2,6 @@ import {SubstrateEvent} from "@subql/types";
 import {StakingReward} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
-function createSumReward(accountId: string): SumReward {
-    const entity = new SumReward(accountId);
-    entity.accountReward = BigInt(0);
-    return entity;
-}
-
 export async function handleStakingRewarded(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, newReward]}} = event;
     const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
