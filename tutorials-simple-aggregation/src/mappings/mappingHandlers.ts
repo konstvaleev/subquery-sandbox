cfc8a2972ae5f9127ee397298fdf170e96a372e9
Valeev <konstantin.valeev@wirecard.com>
2022-01-30 18:57:00 +0300

    added Reward handler (withour -ed)

--- tutorials-simple-aggregation/src/mappings/mappingHandlers.ts
+++ tutorials-simple-aggregation/src/mappings/mappingHandlers.ts
@@ -15,7 +15,7 @@ function createSumReward(accountId: string): SumReward {
     const entity = new SumReward(accountId);
     entity.totalReward = BigInt(0);
     return entity;
-    }
+}
 
 export async function handleSumRewarded(event: SubstrateEvent):
     Promise<void> {
@@ -27,4 +27,12 @@ export async function handleSumRewarded(event: SubstrateEvent):
     entity.totalReward = entity.totalReward + (newReward as Balance).toBigInt();
     entity.blockheight = event.block.block.header.number.toNumber();
     await entity.save();
-    }
+}
+
+export async function handleSumReward(event: SubstrateEvent): Promise<void> {
+    await handleSumRewarded(event)
+}
+
+export async function handleStakingReward(event: SubstrateEvent): Promise<void> {
+    await handleStakingRewarded(event)
+}
