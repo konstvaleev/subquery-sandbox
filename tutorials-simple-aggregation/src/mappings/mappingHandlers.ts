d262f21189e086b11c75d1186e8ff7be0a2f7f5b
Valeev <konstantin.valeev@wirecard.com>
2022-01-30 17:50:51 +0300

    added sumrewarded handler

--- tutorials-simple-aggregation/src/mappings/mappingHandlers.ts
+++ tutorials-simple-aggregation/src/mappings/mappingHandlers.ts
@@ -2,6 +2,23 @@ import {SubstrateEvent} from "@subql/types";
 import {StakingReward} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
+function createSumReward(accountId: string): SumReward {
+    const entity = new SumReward(accountId);
+    entity.totalReward = BigInt(0);
+    return entity;
+    }
+
+export async function handleSumRewarded(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [account, newReward]}} = event;
+    let entity = await SumReward.get(account.toString());
+    if (entity === undefined){
+        entity = createSumReward(account.toString());
+    }
+    entity.totalReward = entity.totalReward + (newReward as Balance).toBigInt();
+    entity.blockheight = event.block.block.header.number.toNumber();
+    await entity.save();
+    }
+
 export async function handleStakingRewarded(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, newReward]}} = event;
     const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
